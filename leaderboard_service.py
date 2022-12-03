acfdedc1b3cb2906dfb73c93b951a448c206f51c
Sam <huy.le09@protonmail.com>
2022-12-03 12:51:37 -0800

    update to take in game result and guess count instead of score

--- leaderboard_service.py
+++ leaderboard_service.py
@@ -14,14 +14,17 @@ async def update_leaderboard():
         return jsonify({username.decode('utf-8'): score for username, score in top10})
     else:
         data = await request.get_json()
-        if not data or 'username' not in data or 'score' not in data:
-            return helpers.jsonify_message("Required username and score"), 400
+        if not data or 'username' not in data or 'is_won' not in data or 'guess' not in data:
+            return helpers.jsonify_message("Required username, is_won(0/1), guess"), 400
         username = data['username']
-        score = int(data['score'])
-        if not r.zscore(players, username):
-            r.zadd(players, {username: 0})
-        old_score = r.zscore(players, username)
-        r.zadd(players, {username: old_score + score})
+        is_won = int(data['is_won'])
+        guess = int(data['guess'])
+        if (is_won):
+            score = app.config["WORDLE"]["MAX_NUM_ATTEMPTS"] - guess + 1
+            if not r.zscore(players, username):
+                r.zadd(players, {username: 0})
+            old_score = r.zscore(players, username)
+            r.zadd(players, {username: old_score + score})
         return {username: r.zscore(players, username)}
 
     

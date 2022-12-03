2c8ad5e69e09f1e7050eca0e73f5a0e072869702
Sam <huy.le09@protonmail.com>
2022-12-03 13:57:41 -0800

    Update to use average score

--- leaderboard_service.py
+++ leaderboard_service.py
@@ -5,6 +5,8 @@ from quart import Quart, jsonify, g, request, abort
 app = Quart(__name__)
 app.config.from_file(f"./config/app.toml", toml.load)
 players = "Players"
+game_count = "GameCount"
+score_sum = "ScoreSum"
 r = redis.StrictRedis(host='localhost',port=6379,db=0)
 
 @app.route("/leaderboard", methods=["GET", "POST"])
@@ -19,12 +21,25 @@ async def update_leaderboard():
         username = data['username']
         is_won = int(data['is_won'])
         guess = int(data['guess'])
-        if (is_won):
-            score = app.config["WORDLE"]["MAX_NUM_ATTEMPTS"] - guess + 1
-            if not r.zscore(players, username):
-                r.zadd(players, {username: 0})
-            old_score = r.zscore(players, username)
-            r.zadd(players, {username: old_score + score})
+        score = app.config["WORDLE"]["MAX_NUM_ATTEMPTS"] - guess + 1 if is_won else 0
+
+        if not r.hget(game_count, username):
+            r.hset(game_count, username, 0)
+        old_game_count = int(r.hget(game_count, username).decode('utf-8'))
+        r.hset(game_count, username, old_game_count + 1)
+
+        if not r.hget(score_sum, username):
+            r.hset(score_sum, username, 0)
+        old_score_sum = int(r.hget(score_sum, username).decode('utf-8'))
+        new_score = old_score_sum + score
+        r.hset(score_sum, username, new_score)
+
+        if not r.zscore(players, username):
+            r.zadd(players, {username: 0})
+        new_game_count = int(r.hget(game_count, username).decode('utf-8'))
+        new_score_sum = int(r.hget(score_sum, username).decode('utf-8'))
+        print(f"Total score: {new_score_sum}, Total played: {new_game_count}")
+        r.zadd(players, {username: new_score_sum / new_game_count})
         return {username: r.zscore(players, username)}
 
     
